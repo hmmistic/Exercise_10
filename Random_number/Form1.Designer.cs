1755d297af88d9b2dd78aacefbf8d208668fbef7
Rəşad <rashadmail@proton.me>
2025-10-29 18:17:41 +0400

    Initial commit

---
+++ Random_number/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Random_number
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            button1 = new Button();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button2 = new Button();
+            richTextBox1 = new RichTextBox();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            groupBox1.ForeColor = Color.White;
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(364, 316);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Ədəd axtarışı";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(224, 224, 224);
+            button1.ForeColor = Color.Black;
+            button1.Location = new Point(57, 202);
+            button1.Name = "button1";
+            button1.Size = new Size(251, 47);
+            button1.TabIndex = 2;
+            button1.Text = "Yoxla";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(57, 77);
+            label1.Name = "label1";
+            label1.Size = new Size(125, 25);
+            label1.TabIndex = 1;
+            label1.Text = "Axtarılan ədəd";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(57, 138);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(251, 31);
+            textBox1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(224, 224, 224);
+            button2.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.Location = new Point(460, 277);
+            button2.Name = "button2";
+            button2.Size = new Size(328, 51);
+            button2.TabIndex = 3;
+            button2.Text = "Yeni oyun";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(460, 23);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(328, 238);
+            richTextBox1.TabIndex = 4;
+            richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Maroon;
+            ClientSize = new Size(800, 340);
+            Controls.Add(richTextBox1);
+            Controls.Add(button2);
+            Controls.Add(groupBox1);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            MaximizeBox = false;
+            Name = "Form1";
+            Text = "Random number";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Button button1;
+        private Label label1;
+        private TextBox textBox1;
+        private Button button2;
+        private RichTextBox richTextBox1;
+    }
+}
---
+++ Random_number/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System.Security.Cryptography;
+
+namespace Random_number
+{
+    public partial class Form1 : Form
+    {
+        private Random random = new Random();
+        private int randomNumber;
+        private int count = 0;
+        private bool canGenerate = true;
+        public Form1()
+        {
+            InitializeComponent();
+            randomNumber = random.Next(1, 101);
+            richTextBox1.Text = "0";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int.TryParse(textBox1.Text, out int guessNumber);
+
+            if (canGenerate == false)
+            {
+                MessageBox.Show("Ədədi yenidən təxmin etmək üçün zəhmət olmasa yeni oyun başladın");
+                return;
+            }
+
+            count++;
+            richTextBox1.Text = count.ToString();
+
+            if (guessNumber == randomNumber)
+            {
+                MessageBox.Show("Siz ədədi düzgün təxmin etdiniz!");
+                canGenerate = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            canGenerate = true;
+            count = 0;
+            richTextBox1.Text = "0";
+            textBox1.Clear();
+            randomNumber = random.Next(1, 101);
+        }
+    }
+}
